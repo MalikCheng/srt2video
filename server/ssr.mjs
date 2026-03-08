0ea47ad16e45bc3ffeebcb83f3817576b9ee98ac
malikcheng <malikcheng@email.com>
2026-03-08 23:22:35 +0800

    fix: add production server for Cloud Run

--- server/ssr.mjs
+++ server/ssr.mjs
@@ -49,8 +49,8 @@ async function createServer() {
   });
 
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`SSR Server running at http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`SSR Server running at http://0.0.0.0:${PORT}`);
   });
 }
 
